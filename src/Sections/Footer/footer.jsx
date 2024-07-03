1a5e01a116a7ddb04c7aa01d824110a7d0e81177
durdure <duresaeshetu2001@gmail.com>
2024-07-03 21:03:56 +0300

    portifolio

--- src/Sections/Footer/footer.jsx
+++ src/Sections/Footer/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
       <section id="footer" className={styles.footerContainer}>
         <p>
-          &copy; 2024 Duresa Eshetu. <br />
+          &copy;2024 Duresa Eshetu.<br />
           All rights reserved.
         </p>
       </section>
